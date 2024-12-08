1ac5cc42a5fc2d93db4436a637a7a29ff39b0527
Hardik Shah <hshah@goodtogreatschools.org.au>
2024-12-08 07:29:00 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'build', // Change this to your desired output path
+        assetsDir: 'assets', // Optional: customize assets directory name
+        manifest: true,
+    },
 });
