1b23db41f4871b9aa5e845544efcd6e311754119
Ghanu119 <ghanshyamashra@gmail.com>
2024-12-28 14:55:57 +0530

    new route added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,6 +54,14 @@ class HomeController extends Controller
         return view('terms');
     }
 
+    public function getInvest(){
+        return view('invest');
+    }
+
+    public function getPrivacyPolicy(){
+        return view('privacy-policy');
+    }
+
     public function getPropertyDetail(){
         return view('property-detail');
     }
--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,7 @@ Route::get('about', [HomeController::class, 'getAbout'])->name('about');
 Route::get('listing', [HomeController::class, 'getListing'])->name('listing');
 Route::get('listing-full', [HomeController::class, 'getListingFull'])->name('listing.full');
 Route::get('contact', [HomeController::class, 'getContact'])->name('contact');
+Route::get('invest', [HomeController::class, 'getInvest'])->name('invest');
 Route::get('terms', [HomeController::class, 'getTerms'])->name('terms');
+Route::get('privacy-policy', [HomeController::class, 'getPrivacyPolicy'])->name('privacy-policy');
 Route::get('property-detail', [HomeController::class, 'getPropertyDetail'])->name('property.detail');
